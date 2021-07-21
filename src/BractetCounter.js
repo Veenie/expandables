85174d7596d2328fcccd76f887c2847da9103413
Veenie <raveen08@gmail.com>
2021-07-21 15:36:18 -0700

    bracket counter added

---
+++ src/BractetCounter.js
@@ -0,0 +1,21 @@
+function BracketMatcher(str) { 
+
+    //account for the unmatched brackets via a count
+    
+      let count = 0;
+    
+    
+    //for loop to account if there is unmatched bracket 
+    //0 will be count if all are matched
+    
+      for (let i = 0; i < str.length; i++) {
+        if (str[i] === '(') count++;
+        if (str[i] === ')') count--;
+        if (count < 0) return 0;
+      }
+    
+    //problem asks for 1 as true result and 0 as false
+    
+      if (count === 0) return 1
+      else return 0
+    }
