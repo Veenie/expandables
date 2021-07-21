0778a36d00065412527f76e0eccc85af082f7665
Veenie <raveen08@gmail.com>
2021-07-20 19:15:37 -0700

    added buton toggle

---
+++ src/Toggle.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { toggle: true  }
+  }
+
+  handleClick() {
+    // todo
+    this.setState({toggle: !this.state.toggle})
+  }
+
+  render() {
+    return (
+      <button onClick={()=> this.handleClick()}>{this.state.toggle ? "ON" : "OFF"}</button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('root')
+);
