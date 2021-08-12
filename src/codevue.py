33f5ea51973759069c50dd48b9373fa81339c596
Veenie <raveen08@gmail.com>
2021-08-12 16:46:12 -0700

    added question 2

--- src/codevue.py
+++ src/codevue.py
@@ -2,3 +2,10 @@
 
 # for line in sys.stdin:
 #     print(int(line)*int(line))
+
+
+# for line in sys.stdin:
+#     if 'test case 3' in line:
+#         print('This is the expected Output. Try changing the code to write this to STDOUT for this case.')
+#     else:
+#         print (line)    
