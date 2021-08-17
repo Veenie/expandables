2b23982066660a436b921f2bad96d39f9fbd3535
Veenie <raveen08@gmail.com>
2021-08-16 18:18:30 -0700

    added solution for similar problem, account for the least times"

--- src/occur_3_times.js
+++ src/occur_3_times.js
@@ -1,13 +1,34 @@
-var myArray = ["hi", "hi", "hi", "ha", "ha", "ho", "hu", "hu", "hu"]
+// *Write a function that given a list of countries, returns the country(s) that occur the least times
+// i.e - ["Argentina", "Cuba", "Chile", "Argentina"] -> ["Cuba", "Chile"] -----------***
 
-var filteredArray = myArray.filter(item => {
-  //check what the count so far is
-  var count = this[item] ? this[item] : 0;
-  //add one and assign it back as the new count
-  this[item] = ++count;
+
+// var myArray = ["hi", "hi", "hi", "ha", "ha", "ho", "hu", "hu", "hu"]
+
+// var filteredArray = myArray.filter(item => {
+//   //check what the count so far is
+//   var count = this[item] ? this[item] : 0;
+//   //add one and assign it back as the new count
+//   this[item] = ++count;
   
-  //only return the item if the count is 3
-  return count === 3;
- }, {})
+//   //only return the item if the count is 3
+//   return count === 3;
+//  }, {})
  
-console.log(filteredArray)
+// console.log(filteredArray)
+
+// *Wrinte a function that given a list of countries, returns the country(s) that occur the least times
+// i.e- ["Argentina", "Cuba", "Chile", "Argentina"]->["Cuba", "Chile"]
+
+
+
+// var lowFilteredArray = myArray.filter(item => {
+//   //check what the count so far is
+//   var count = this[item] ? this[item] : 0;
+//   //add one and assign it back as the new count
+//   this[item] = ++count;
+  
+//   //only return the item if the count is 3
+//   return count === 1;
+//  }, {})
+ 
+// console.log(lowFilteredArray)
