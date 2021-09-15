8aa4310e79ed28fb4bb7b272df7b66f74788ff94
Veenie <raveen08@gmail.com>
2021-09-15 13:53:43 -0700

    time complexity example for blog

---
+++ src/time_complexity.js
@@ -0,0 +1,10 @@
+function stringIncludes(wrd, ltr){
+    let match;
+    for(let i = 0; i < wrd.length; i++){
+      if(wrd[i] === ltr){
+    match = true
+  }
+      else{match = false}
+}
+return match
+  }
