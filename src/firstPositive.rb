7772a7cc0bd4be1a12820fbe4ddfde66047232c0
Veenie <raveen08@gmail.com>
2021-11-17 12:36:09 -0800

    explanation

--- src/firstPositive.rb
+++ src/firstPositive.rb
@@ -1,8 +1,13 @@
 def first_missing_positive(nums)
     i = 1
+
+    #default answer is 1, set variable to that
+
     while nums.include?(i)
         i += 1
     end
+
+    #include searches for 1, if it's there, i becomes 2 and so forth until the missing positive is hit 
     
     i
 end
