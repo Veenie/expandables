98a742a28adae5ba1d84ee86438cc4111d198879
Veenie <raveen08@gmail.com>
2021-11-30 14:10:22 -0800

    java fizz buzz

---
+++ src/javaFizzBuzz.rb
@@ -0,0 +1,28 @@
+# package com.cognixia.jump.corejava.flowcontrol;
+
+# public class FizzBuzz {
+
+# 	public static void main(String[] args) {
+# 		// TODO Auto-generated method stub
+# 		int n = 45;
+# 		for(int i = 1; i <= n; i++) {
+# 			if(i%15 == 0) {
+# 				System.out.println("FizzBuzz");
+# 			}	
+# 			else if(i%5 == 0) {
+# 				System.out.println("Fizz");
+# 			}
+# 			else if(i%3 == 0) {
+# 				System.out.println("Buzz");
+# 			}
+# 			else if(i%2 == 0 && i != 2 || i == 1) {
+# 				System.out.println(i);
+# 			} 
+# //			else
+# //				System.out.println(i);
+# //			if you don't want to skip primes like above
+# 		}
+
+# 	}
+
+# }
