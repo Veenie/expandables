f0181cb3a45ea33f7d65c1cd6b8acb8e3a221dd5
Veenie <raveen08@gmail.com>
2021-11-29 19:36:44 -0800

    java scanner code

---
+++ src/javaReadfromConsole.rb
@@ -0,0 +1,17 @@
+# package com.cognixia.jump.corejava.firstprogram;
+
+# import java.util.Scanner;
+
+# public class ReadFromConsole {
+
+# 	public static void main(String[] args) {
+# 		// TODO Auto-generated method stub
+# 		Scanner input = new Scanner(System.in);
+# 		System.out.println("Please enter an input value");
+# 		String storedUserValue = input.nextLine();
+# 		System.out.println("You entered " + storedUserValue);
+# 		input.close();
+
+# 	}
+
+# }
