9a873c8c374395f5be8ae807c857ffb16ac080b7
Veenie <raveen08@gmail.com>
2021-12-01 14:08:07 -0800

    set up animal runner

--- src/javaAnimalClasses.rb
+++ src/javaAnimalClasses.rb
@@ -1,127 +1,156 @@
-package com.cognixia.jump.corejava.animals;
+# package com.cognixia.jump.corejava.animals;
 
-public class Animal {
+# public class Animal {
 
-	// Attributes
-	private String name;
-	private String species;
+# 	// Attributes
+# 	private String name;
+# 	private String species;
 	
 	
 	
-	// Constructors
+# 	// Constructors
 	
-	public Animal() {
-		super();
-	}
+# 	public Animal() {
+# 		super();
+# 	}
 	
-	public Animal(String name, String species) {
-		super();
-		this.name = name;
-		this.species = species;
-	}
-	
-	
-	
-	// Methods
-	
-	public void sound() {
-		System.out.println("The animal made a sound!!");
-	}
+# 	public Animal(String name, String species) {
+# 		super();
+# 		this.name = name;
+# 		this.species = species;
+# 	}
 	
 	
 	
-	// Getters & Setters
+# 	// Methods
 	
-	public String getName() {
-		return this.name;
-	}
+# 	public void sound() {
+# 		System.out.println("The animal made a sound!!");
+# 	}
 	
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-	public String getSpecies() {
-		return species;
-	}
+	
+	
+# 	// Getters & Setters
+	
+# 	public String getName() {
+# 		return this.name;
+# 	}
+	
+# 	public void setName(String name) {
+# 		this.name = name;
+# 	}
 
 
-	public void setSpecies(String species) {
-		this.species = species;
-	}
+# 	public String getSpecies() {
+# 		return species;
+# 	}
 
-	@Override
-	public String toString() {
-		return "Animal [name=" + name + ", species=" + species + "]";
-	}
+
+# 	public void setSpecies(String species) {
+# 		this.species = species;
+# 	}
+
+# 	@Override
+# 	public String toString() {
+# 		return "Animal [name=" + name + ", species=" + species + "]";
+# 	}
 	
 
-}
+# }
 
-package com.cognixia.jump.corejava.animals;
+# package com.cognixia.jump.corejava.animals;
 
-public class Sloth extends Animal{
+# public class Sloth extends Animal{
 	
-	private int speed;
+# 	private int speed;
 	
-	public Sloth() {
-		super();
-	}
+# 	public Sloth() {
+# 		super();
+# 	}
 	
-	public Sloth(String name, String species, int speed) {
-		super(name, species);
-		this.speed = speed;
-	}
+# 	public Sloth(String name, String species, int speed) {
+# 		super(name, species);
+# 		this.speed = speed;
+# 	}
 	
-	@Override
-	public void sound() {
-		System.out.println("The sloth goes squeak!!!");
-	}
+# 	@Override
+# 	public void sound() {
+# 		System.out.println("The sloth goes squeak!!!");
+# 	}
 	
-	public int getSpeed() {
-		return speed;
-	}
+# 	public int getSpeed() {
+# 		return speed;
+# 	}
 
-	public void setSpeed(int speed) {
-		this.speed = speed;
-	}
+# 	public void setSpeed(int speed) {
+# 		this.speed = speed;
+# 	}
 
-}
+# }
 
-package com.cognixia.jump.corejava.animals;
+# package com.cognixia.jump.corejava.animals;
 
-public class Cat extends Animal{
-    private boolean tail;
+# public class Cat extends Animal{
+#     private boolean tail;
 	
-	public Cat() {
-		super();
-	}
+# 	public Cat() {
+# 		super();
+# 	}
 	
-	public Cat(String name, String species, boolean tail) {
-		super(name, species);
-		this.tail = tail;
-	}
+# 	public Cat(String name, String species, boolean tail) {
+# 		super(name, species);
+# 		this.tail = tail;
+# 	}
 	
-	@Override
-	public void sound() {
-		System.out.println("The cat goes meow!");
-	}
+# 	@Override
+# 	public void sound() {
+# 		System.out.println("The cat goes meow!");
+# 	}
 	
-	public boolean getTail() {
-		return tail;
-	}
+# 	public boolean getTail() {
+# 		return tail;
+# 	}
 
-	public void setTail(boolean tail) {
-		this.tail = tail;
-	}
+# 	public void setTail(boolean tail) {
+# 		this.tail = tail;
+# 	}
 	
-	public void tailStatus() {
-		if(this.tail == true) {
-			System.out.println(this.getName() + " has a tail!");
-		}
-		else
-			System.out.println(this.getName() + " has no tail!");	
-	}
+# 	public void tailStatus() {
+# 		if(this.tail == true) {
+# 			System.out.println(this.getName() + " has a tail!");
+# 		}
+# 		else
+# 			System.out.println(this.getName() + " has no tail!");	
+# 	}
 	
 	       
-}
+# }
+
+# package com.cognixia.jump.corejava.animals;
+
+# public class AnimalRunner {
+
+# 	public static void main(String[] args) {
+# 		Animal animal1 = new Animal();
+# 		Animal animal2 = new Animal("Foo Bar", "Mammal");
+		
+# 		Sloth sloth1 = new Sloth();
+# 		Sloth sloth2 = new Sloth("Baz Buzz", "Mammal", 2);
+		
+# 		Cat cat1 = new Cat("Arbuckle", "Mammal", false);
+		
+		
+# 		animal1.sound();
+# 		System.out.println("'animal2's name is ---> " + animal2.getName());
+		
+		
+# 		sloth1.sound();
+# 		System.out.println("'sloth2's name is --> " + sloth2.getName());
+# 		System.out.println("'sloth2's speed is --> " + sloth2.getSpeed() + "cm per hour!!!");
+		
+# 		System.out.println("This pet's name is --> " + cat1.getName());
+# 		System.out.println(cat1.getName() + " is a --> " + cat1.getSpecies());
+# 		cat1.sound();
+# 		cat1.tailStatus();
+# 	}
+# }
