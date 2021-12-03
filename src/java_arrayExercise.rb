c3504a267e68f2ad850a2bc467ff930b026ee12a
Veenie <raveen08@gmail.com>
2021-12-02 22:56:43 -0800

    java array matcher

---
+++ src/java_arrayExercise.rb
@@ -0,0 +1,24 @@
+# package com.cognixia.jump.corejava.staticexample;
+
+# import java.util.Arrays; 
+
+# public class ArrayExcersise {
+
+	
+# 	public static void main(String[] args) {
+# 		// TODO Auto-generated method stub
+# 		int[] arr1 = {1, 2, 3};
+# 		int[] arr2 = {1, 3, 2};
+		
+# 		Arrays.sort(arr1);
+# 		Arrays.sort(arr2);
+		
+# 		if (Arrays.equals(arr1, arr2)) {
+# 			System.out.println("The arrays are equal");
+# 		}
+# 		else
+# 			System.out.println("The arrays are not equal");
+
+# 	}
+
+# }
