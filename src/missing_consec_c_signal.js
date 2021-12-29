4be6fa428cd6483806caffe11f87b3d4fd865d5f
Veenie <raveen08@gmail.com>
2021-12-28 19:35:26 -0800

    code sig consec

---
+++ src/missing_consec_c_signal.js
@@ -0,0 +1,20 @@
+
+function solution(statues) {
+
+
+    return (Math.max(...statues) - Math.min(...statues) + 1) - statues.length
+    
+    //calculate how many consecutive numbers should be there then subtract the number of values that are actually there
+    
+    }
+    
+    //java
+    
+    // int solution(int[] statues) {
+    // int count = 0;
+    //    Arrays.sort(statues);
+    //    for(int i=1; i<statues.length; i++) {
+    //       count += statues[i] - statues[i-1] - 1;
+    //    }
+    //    return count;
+    // }
